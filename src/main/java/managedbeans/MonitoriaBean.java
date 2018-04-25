286e068a8668238fa3eed3fa36288762ad0c205b
carlos castaneda <carlos.castaneda-l@mail.escuelaing.edu.co>
2018-04-25 12:06:13 -0500

    beans

---
+++ src/main/java/managedbeans/MonitoriaBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+import entidades.Estudiante;
+import logica.servicios.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+/**
+ *
+ * @author 2110540
+ */
+@ManagedBean(name = "mb")
+@SessionScoped
+public class MonitoriaBean {
+    
+}
