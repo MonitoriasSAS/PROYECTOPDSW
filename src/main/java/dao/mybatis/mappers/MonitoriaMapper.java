5a4b4e90d87ffb81fb95fa7163cb27a9c03640f6
alejandrordto <masterale2011@gmail.com>
2018-04-24 22:23:06 -0500

    adelanto_de_hoy

---
+++ src/main/java/dao/mybatis/mappers/MonitoriaMapper.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao.mybatis.mappers;
+
+import entidades.Monitoria;
+
+import org.apache.ibatis.annotations.Param;
+/**
+ *
+ * @author MonitoriasSaS
+ */
+public interface MonitoriaMapper{
+    public Monitoria loadMonitoria(@Param("idmo") int id);
+    public void agregarMonitoria(@Param("ip") String ip, 
+            @Param("obvs") String obvservaciones,@Param("idcar") int id ,
+            @Param("name") String nombre, @Param("last") String apellido);
+}
