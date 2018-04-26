16abe5cc83ac1b39947d6e9ff2c7dae6b19ab354
alejandrordto <masterale2011@gmail.com>
2018-04-25 19:02:10 -0500

    test?

---
+++ src/test/java/Test/SimpleTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import logica.servicios.serviciosMonitoria;
+import logica.servicios.serviciosMonitoriaFactory;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ *
+ * @author monitoriaSAS
+ */
+public class SimpleTest {
+    public SimpleTest(){
+    }
+    @Test
+    public void deberiaAgregarUnaMonitoria() {
+        //serviciosMonitoria serv= serviciosMonitoriaFactory.getInstance();
+        //serv.registrarMonitoria();
+        //assertEquals();
+    }
+}
