cb31698ec1e4c46a4e9969f5d39e268ae0baecd1
nicolaspatino <nicolas.patino@mail.escuelaing.edu.co>
2018-05-03 12:50:48 -0500

    servicios,conexion base de datos, mybatis

---
+++ src/main/java/logica/servicios/ServiciosX.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica.servicios;
+
+/**
+ *
+ * @author nicolas
+ */
+public class ServiciosX {
+    //completar con los servicios par alas monitorias
+}
