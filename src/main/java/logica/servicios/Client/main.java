f02c1c278d6c1c3cb2e65625fcae1a09e7874f00
alejandrordto <masterale2011@gmail.com>
2018-05-09 08:41:03 -0500

    arreglando recourses y alias

--- src/main/java/logica/servicios/Client/main.java
+++ src/main/java/logica/servicios/Client/main.java
@@ -21,11 +21,13 @@ public class main {
     private static SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH::SS");
     private static Date fechaPru ;
     public static void main(String a[]){
+         System.out.println("not yet");
          try {
             fechaPru=sdf.parse("2018-01-10 7:00:00");
         } catch (ParseException ex) {
             Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
         }
+         System.out.println("not yet");
          serviciosMonitoriaFactory.getInstance().getServiciosMonitoria().agregarSemestre("2018-2",fechaPru,fechaPru);
          System.exit(0);
     }
