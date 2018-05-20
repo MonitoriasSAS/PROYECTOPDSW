9c522b4b70f1a8b2a9b632ffa75a0a35901e4e96
andresrdt <andres.rodriguez-de@mail.escuelaing.edu.co>
2018-05-20 00:36:36 -0500

    correcion ups

--- src/main/java/edu/eci/pdsw/dao/mybatis/mappers/ProfesorMapper.java
+++ src/main/java/edu/eci/pdsw/dao/mybatis/mappers/ProfesorMapper.java
@@ -18,7 +18,7 @@ public interface ProfesorMapper{
     float consultarEstudiantes(@Param("grup")int grupo);
     List<Monitoria> consultarMonitorias(@Param("idasi") int asi);
     List<Monitoria> consultarTotalMonitorias(@Param("idpro")int id,@Param("idasi") int asi);
-    List<Estudiante> consulatarEstudiantesCursos(@Param("idpro")int id,@Param("idgrupo")int numgrupo,@Param("idasi")int numasig);
+    Grupo consulatarEstudiantesCursos(@Param("idpro")int id,@Param("idgrupo")int numgrupo,@Param("idasi")int numasig);
     List<Monitoria> consultarMonitoriasCursos(@Param("idpro")int id,@Param("idgrupo")int numgrupo,@Param("idasi")int numasig);
     List<Grupo> consultarCursos(@Param("idpro")int id,@Param("idasi")int numasig);
     List<Monitoria> consultarEstudiantesGrupo(@Param("idpro")int id,@Param("idgrupo")int numgrupo,@Param("idasi")int numasig);
