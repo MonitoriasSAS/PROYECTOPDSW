92c130c9a707da7c20e5789e93a6c4e832a86f51
nicolaspatino <31293381+nicolaspatino@users.noreply.github.com>
2018-05-22 23:12:52 -0500

    Update ShiroLoginBean.java

--- src/main/java/edu/eci/pdsw/seguridad/bean/ShiroLoginBean.java
+++ src/main/java/edu/eci/pdsw/seguridad/bean/ShiroLoginBean.java
@@ -74,6 +74,11 @@ public class ShiroLoginBean implements Serializable {
             facesError("Unknown error: " + ex.getMessage());
             log.error(ex.getMessage(), ex);
         }
+         catch (IOException ex){
+            facesError("Unknown error: " + ex.getMessage());
+            log.error(ex.getMessage(), ex);
+            
+        }
         finally {
             token.clear();
         }
